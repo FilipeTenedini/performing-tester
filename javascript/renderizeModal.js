74eeb7bcbcdc009b4ab61663cc7bf5c45e6da8e6
filipetenedini <tenedinifilipe@gmail.com>
2023-07-18 09:56:46 -0300

    feat: add render and remove modal

--- javascript/renderizeModal.js
+++ javascript/renderizeModal.js
@@ -1,1 +1,21 @@
-console.log(-17.8)
+function renderModal() {
+    document.querySelector('body').innerHTML+= `
+    <div class="modal-area--backdrop" onclick="removeModal()">
+        <div class="modal-area">
+            <div class="modal-header">
+
+            </div>
+            <div class="modal-content">
+
+            </div>
+            <div class="modal-footer">
+
+            </div>
+        </div>
+    </div>
+    `;
+}
+function removeModal() {
+    const modal = document.querySelector('.modal-area--backdrop');
+    modal.parentNode.removeChild(modal);
+}

f11acb56f3d8a08b695e38c5e3203e3f7e1103d6
filipetenedini <tenedinifilipe@gmail.com>
2023-07-18 12:05:27 -0300

    structure modal header

--- javascript/renderizeModal.js
+++ javascript/renderizeModal.js
@@ -1,5 +1,9 @@
+function blockScroll() {
+    document.body.classList.add('blocking-scroll');
+}
 function renderModal() {
-    document.querySelector('body').innerHTML+= `
+    const body = document.querySelector('body');
+    body.innerHTML+= `
     <div class="modal-area--backdrop" onclick="removeModal()">
         <div class="modal-area">
             <div class="modal-header">
@@ -14,8 +18,10 @@ function renderModal() {
         </div>
     </div>
     `;
+    blockScroll();
 }
 function removeModal() {
     const modal = document.querySelector('.modal-area--backdrop');
+    document.body.classList.remove('blocking-scroll');
     modal.parentNode.removeChild(modal);
 }

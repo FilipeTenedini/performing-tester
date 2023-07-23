bd077f852b565350f98cb6b216434c013023f6cd
filipetenedini <tenedinifilipe@gmail.com>
2023-07-23 15:03:16 -0300

    feat: add action to coupons buttons

--- javascript/events-click.js
+++ javascript/events-click.js
@@ -9,6 +9,12 @@ function toggleDropdownMenu(navItem) {
 function blockScroll() {
     document.body.classList.add('blocking-scroll');
 }
+function showModal() {
+    window.open('http://127.0.0.1:5500/index.html#', '_blank');
+    const modal = document.querySelector('.modal-area--backdrop');
+    modal.remove('non-display');
+    blockScroll();
+}
 function removeModal() {
     const modal = document.querySelector('.modal-area--backdrop');
     document.body.classList.remove('blocking-scroll');
